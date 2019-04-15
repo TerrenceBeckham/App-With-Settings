1b8ee33fd19f60d09e933f90936fdc14174adbfb
Terrence Beckham <https://github.com/TerrenceBeckham>
2019-04-15 08:43:33 +0200

    Connected the Settings menu item to Settings Activity.

--- app/src/main/java/com/afrodroid/appwithsettings/SettingsActivity.java
+++ app/src/main/java/com/afrodroid/appwithsettings/SettingsActivity.java
@@ -8,6 +8,7 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         //Display Fragment as main content.
         getSupportFragmentManager().beginTransaction().replace(android.R.id.content,
                 new SettingsFragment()).commit();
